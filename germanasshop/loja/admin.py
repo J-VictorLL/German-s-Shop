464dc666390e8d29635384ef5dfab5d3b00aa5b6
Fernando <fernandosmartphone96@gmail.com>
2022-03-30 09:33:43 -0300

    cria o model Favorito

--- germanasshop/loja/admin.py
+++ germanasshop/loja/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Reclamacao, Produto
+from .models import Favorito,Reclamacao, Produto
 
 admin.site.register(Produto)
 admin.site.register(Reclamacao)
+admin.site.register(Favorito)
 
 class ProdutoAdmin(admin.ModelAdmin):
     list_dispaly = ('nome', 'descricao', 'preco', 'url_imagem')
--- germanasshop/loja/models.py
+++ germanasshop/loja/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from django.db import models
 
+from cliente.models import Usuario
+
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=7, decimal_places=2)
@@ -20,3 +22,9 @@ class Reclamacao(models.Model):
 
     def __str__(self):
         return str(self.pk) + ' ' + self.titulo
+
+class Favorito(models.Model):
+    id_usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+    id_produto = models.ForeignKey(Produto, on_delete = models.CASCADE)
+    data_adicao = models.DateTimeField('Data da Adição na lista',default=datetime.now())
+    #Como juntar as duas chaves pra não haver repetição
