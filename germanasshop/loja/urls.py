b4b08969530e6ae95c1571b1ccfe8016f9800d33
João Victor <jhonveyne@gmail.com>
2022-03-31 22:16:11 -0300

    adicionar botão

--- germanasshop/loja/urls.py
+++ germanasshop/loja/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('forum/', views.forum, name='forum'),
     path('cesta/', views.cesta, name='cesta'),
     path('produto/<int:id_produto>/', views.produto, name='produto'),
+    path('historico/', views.historico, name='historico'),
 ]
